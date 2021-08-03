98180106438deed225a166e768d1f7734ddf18de
danbe80 <danbe8080@gmail.com>
2021-08-03 16:52:12 +0900

    fade slide 완료

--- js/index.js
+++ js/index.js
@@ -20,3 +20,15 @@ navi [4].addEventListener('mouseleave', onLeave);
 function onEnter(){  navimeun[0].style.display ="block";}
 function onLeave(){  navimeun[0].style.display = "none";} 
 */
+
+var index = 0;
+const slides = document.getElementsByClassName('img-wrap');
+
+setInterval(function(){
+	for(let i = 0; i < slides.length; i++){
+		slides[i].style.opacity = 0;
+	}
+	index = (index != slides.length - 1) ? index + 1 : 0;
+	slides[index].style.opacity = 1;
+}, 3000);
+
